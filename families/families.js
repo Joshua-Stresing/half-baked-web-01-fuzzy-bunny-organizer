2e80912fbf01885b2b767b77a9b22d0d5eceb934
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-21 13:10:14 -0800

    yolo on this family js nonsense

--- families/families.js
+++ families/families.js
@@ -9,32 +9,48 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+// create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
+// add the bunnies css class to the bunnies el, and family css class to the family el
+// put the family name in the name element
+// for each of this family's bunnies
+//    make an element with the css class 'bunny', and put the bunny's name in the text content
+//    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
+// append this bunnyEl to the bunniesEl
+function displayFamilies(families) {
     // fetch families from supabase
-
     // clear out the familiesEl
-
+    
     for (let family of families) {
-        // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
-        // your HTML Element should look like this:
-        // <div class="family">
-        //    <h3>the Garcia family</h3>
-        //    <div class="bunnies">
-        //        <div class="bunny">Fluffy</div>
-        //        <div class="bunny">Bob</div>
-        //    </div>
-        // </div>
-        // add the bunnies css class to the bunnies el, and family css class to the family el
-        // put the family name in the name element
-        // for each of this family's bunnies
-        //    make an element with the css class 'bunny', and put the bunny's name in the text content
-        //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
-        // append this bunnyEl to the bunniesEl
+        const familyEl = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const bunniesEl = document.createElement('div');
+    
+        bunniesEl.classList.add('bunnies');
+        familyEl.classList.add('family');
+    
+        nameEl.textContent.textContent = family.name;
+
+        for (let bunny of family.fuzzy_bunnies) {
+            const bunnyEl = document.createElement('div');
+
+            bunnyEl.classList.add('bunny');
+            bunnyEl.textContent = bunny.name;
+
+            bunnyEl.addEventListener('click', async () => {
+                await deleteBunny(bunny.id);
+
+                const updatedFamilies = await getFamilies();
+
+                displayFamilies(updatedFamilies);
+            });
+            bunniesEl.append(bunnyEl);
+        }
+        // append the bunniesEl and nameEl to the familyEl
+        // append the familyEl to the familiesEl
+        familyEl.append(nameEl, bunniesEl);
+        familiesEl.append(familyEl);
     }
 
-    // append the bunniesEl and nameEl to the familyEl
-
-    // append the familyEl to the familiesEl
 }
 
 window.addEventListener('load', async () => {
